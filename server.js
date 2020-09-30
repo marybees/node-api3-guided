34c8c0cd651a43718c8b43f878f4614db8d1ead8
Mary Baker <meb601@gmail.com>
2020-09-30 12:41:15 -0400

    Install dependencies, morgan, initiate morgan

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const morgan = require("morgan"); // remember to npm i morgan
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router");
 
 const server = express();
 
-server.use(express.json());
+const logger = morgan("combined");
 
-server.use('/api/hubs', hubsRouter);
+// middleware
+server.use(express.json()); // built-in
+server.use(logger); // installed 3rd party: npm i morgan
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+// endpoints
+server.use("/api/hubs", hubsRouter);
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? `${req.name}` : "";
+});
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
